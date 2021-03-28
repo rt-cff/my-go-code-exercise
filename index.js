c5b728cea36f4cf9da7fc4c4b07846d3d0e4629d
Raymond Tam <raymondtamtwtp@gmail.com>
2021-03-29 00:13:49 +0800

    feat: implemented transform functionality with test

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-exports.default = () => {
-  return true;
+const getKey = (val) => {
+  const [key] = Object.keys(val);
+
+  return key;
+};
+
+const parse = (val, res) => {
+  if (typeof val === "object") {
+    const key = getKey(val);
+
+    return parse(val[key], {
+      [key]: res,
+    });
+  } else {
+    return {
+      [val]: res,
+    };
+  }
+};
+
+exports.default = (value) => {
+  const key = getKey(value);
+
+  return parse(value[key], key);
 };
--- index.test.js
+++ index.test.js
@@ -1,8 +1,28 @@
 const assert = require("assert");
 const transform = require("./index").default;
 
-describe("Test", function () {
-  it("should return true", function () {
-    assert.equal(transform(), true);
+const DEFAULT_INPUT = {
+  hired: {
+    be: {
+      to: {
+        deserve: "I",
+      },
+    },
+  },
+};
+
+const EXPECTED_OUTPUT = {
+  I: {
+    deserve: {
+      to: {
+        be: "hired",
+      },
+    },
+  },
+};
+
+describe("With default input", function () {
+  it("should return expected output", function () {
+    assert.notStrictEqual(transform(DEFAULT_INPUT), EXPECTED_OUTPUT);
   });
 });
